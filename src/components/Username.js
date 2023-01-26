03ec86e55aa3fdc14f94fd08abbd37fbd47cd110
ankur12v <65348445+ankur12v@users.noreply.github.com>
2023-01-26 22:35:25 +0530

    client started

---
+++ src/components/Username.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Username() {
+  return (
+    <div className="container mx-auto">
+      <div className="flex justify-center items-center h-screen">
+        <div>
+          <div className="title flex flex-col items-center">
+            <h4 className="text-5xl">Hello again!</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
