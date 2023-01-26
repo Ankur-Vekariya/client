c3fca026b5f3a6a8c660adfd60a79358e1a8b662
ankur12v <65348445+ankur12v@users.noreply.github.com>
2023-01-26 23:39:10 +0530

    Login page UI

--- src/components/Username.js
+++ src/components/Username.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import avatar from "../assetes/profile.png";
+import styles from "../styles/Usename.module.css";
 
 export default function Username() {
   return (
     <div className="container mx-auto">
-      <div className="flex h-screen">
-        <div>
+      <div className="flex justify-center items-center h-screen">
+        <div className={styles.glass}>
           <div className="title flex flex-col items-center">
             <h4 className="text-5xl font-bold">Hello again!</h4>
             <span className="py-4 text-xl w-2/3 text-center">
@@ -14,11 +16,17 @@ export default function Username() {
           </div>
           <form className="py-1">
             <div className="profile flex justify-center py-4">
-              <img src="" alt="avatar" />
+              <img src={avatar} alt="avatar" className={styles.profile_img} />
             </div>
-            <div className="textbox flex flex-col items-center py-4">
-              <input type="text" placeholder="Username" />
-              <button type="submit">Let's go</button>
+            <div className="textbox flex flex-col items-center py-4 ">
+              <input
+                type="text"
+                placeholder="Username"
+                className={styles.textbox}
+              />
+              <button type="submit" className={styles.btn}>
+                Let's go
+              </button>
             </div>
             <div className="text-center py-4">
               <span className="text-gray-500">
